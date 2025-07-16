17880a271562d930fa72e4af512abb98f7f59685
Anas Khan <anaskhan995620@gmail.com>
2025-07-17 01:26:35 +0530

    Added Vercel frontend URL to CORS

--- app.js
+++ app.js
@@ -7,7 +7,7 @@ const app = express()
 
 // using middlewares
 
-const allowedOrigins = ["http://localhost:5173", "https://your-frontend-deployed-url.com"];
+const allowedOrigins = ["http://localhost:5173", "https://portfolio-frontend-ruby-rho.vercel.app"];
 
 app.use(cors({
     origin: function (origin, callback) {
